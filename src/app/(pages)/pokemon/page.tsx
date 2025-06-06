572bdb7245286f429a1b33d7c7d315a82031928b
ehdrjs4502 <ehdrjs4502@naver.com>
2025-06-06 20:59:10 +0900

    fix: prefetch query

--- src/app/(pages)/pokemon/page.tsx
+++ src/app/(pages)/pokemon/page.tsx
@@ -4,21 +4,19 @@ import { pokemonListWithQuery } from '@/app/_api/pokemon';
 import { getQueryClient } from '@/app/tanstack-query/get-query-client';
 import PokemonList from './_components/list/list';
 
-const queryClient = getQueryClient();
-
-// 포켓몬 리스트 미리 패칭
-await queryClient.prefetchQuery(pokemonListWithQuery());
-
-const dehydratedState = dehydrate(queryClient);
-
 export default async function PokemonPage() {
-  <HydrationBoundary state={dehydratedState}>
-    <PokemonList />
-  </HydrationBoundary>;
+  const queryClient = getQueryClient();
+
+  // 포켓몬 리스트 미리 패칭
+  await queryClient.prefetchQuery(pokemonListWithQuery());
+
+  const dehydratedState = dehydrate(queryClient);
 
   return (
     <main>
-      <PokemonList />
+      <HydrationBoundary state={dehydratedState}>
+        <PokemonList />
+      </HydrationBoundary>
     </main>
   );
 }
